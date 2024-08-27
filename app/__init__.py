19a631946a9589fc497eadb1882cffc161c6e729
SA1-19 <115571511+SA1-19@users.noreply.github.com>
2024-08-27 15:10:56 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 # app/__init__.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 from flask_migrate import Migrate
 
 app = Flask(__name__)
@@ -11,16 +10,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'  # Define the view for login
-
-# Import the User model
-from app.models import User
-
-# User loader callback
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 # Import views to register routes
 from app import views, models
